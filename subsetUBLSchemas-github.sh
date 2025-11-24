3a0c1627d6fde671e56ec125f7a43428c8f8a695
jaglakiewicz <85844727+jaglakiewicz@users.noreply.github.com>
2025-11-24 21:58:33 +0100

    Invoke XRechnung script in GitHub action

--- subsetUBLSchemas-github.sh
+++ subsetUBLSchemas-github.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#
-# This is the invocation that happens in the GitHub action ... it must be bash
-#
-bash subsetUBLSchemas-DemoUBL21Subset.sh "$1" "$2" "$3" "$4"
+bash subsetUBLSchemas-XRechnung.sh "$1" "$2" "$3" "$4"
